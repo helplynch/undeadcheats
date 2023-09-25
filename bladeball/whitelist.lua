424589e3d84f6ca777eb153af2c90fdb94936aa2
helplynch <108618400+helplynch@users.noreply.github.com>
2023-09-24 22:34:46 -0400

    Update whitelist.lua

--- bladeball/whitelist.lua
+++ bladeball/whitelist.lua
@@ -1,5 +1,7 @@
+print("Whitelist systems are running")
 local verifiedusers = {
   ["FunnyNitron"] = true,
   ["lp0nda"] = true,
-  ["soulstraker"] = true
+  ["soulstraker"] = true,
+  ["ezdubs282"] = true
 }
